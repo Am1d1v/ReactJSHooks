14dd45d743d36f30975030af88ecd428e989ecb7
Am1d1v <dimadiman1adima@gmail.com>
2024-05-04 15:06:16 +0300

    React Hooks. useState

--- src/Hooks/State.jsx
+++ src/Hooks/State.jsx
@@ -1,13 +1,44 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function State() {
 
-    const [value, setValue] = useState(0);
-    
+  const [state, setState] = useState({
+    count: 0,
+    isCounting: false
+  })
 
-    return (
-      <button onClick={() => setValue(prev => prev + 1)}>{value}</button>
-    )
+  const [value, setValue] = useState(() => {
+    const userCount = localStorage.getItem('count');
+    return +userCount || 0
+  });
+
+  useEffect(() => {
+  console.log(state); 
+  }, [state])
+
+  const handleCount = () => {
+   setState({
+    ...state,
+    count: state.count + 1
+   });
+  }
+
+  const handleStatus = () => {
+    setState({
+      ...state,
+      isCounting: !state.isCounting
+    });
+  }
+  
+  return (
+    <div>
+      <button onClick={handleCount}>Increase</button>
+      <button onClick={handleStatus}>Status</button>
+      <h3>Total: {state.count}</h3>
+      <h3>Status: {(state.isCounting).toString()}</h3>
+    </div>
+  )
+
 }
 
 export default State
