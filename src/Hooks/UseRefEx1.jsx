607a04176b3639a4c38321afc9dff7baa3fa0f13
Am1d1v <dimadiman1adima@gmail.com>
2024-05-05 13:31:13 +0300

    React Hooks. useRef() Example 1.

--- src/Hooks/UseRefEx1.jsx
+++ src/Hooks/UseRefEx1.jsx
@@ -3,12 +3,22 @@ import {useRef} from 'react'
 function UseRefEx1() {
 
   const inputReference = useRef();
+  const pararef = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    inputReference.current.value = 'Input Value';
+    inputReference.current.style.backgroundColor = 'black';
+    inputReference.current.style.color = 'white';
+    pararef.current.innerText = 'Text';
+  }
 
   return (
-    <form action="">
+    <form onSubmit={onSubmit}>
       <label htmlFor="name">Name</label>
-      <input type="text" className='form-control mb-2' id='name'/>
+      <input type="text" className='form-control mb-2' id='name' ref={inputReference}/>
       <button type="submit" className='btn btn-primary'>Submit</button>
+      <p ref={pararef} onClick={() => inputReference.current.focus()}></p>
     </form>
   )
 }
