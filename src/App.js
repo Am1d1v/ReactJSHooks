783bdd46355d459bfcb333faae4febdbf3fc0b29
Am1d1v <dimadiman1adima@gmail.com>
2024-05-17 20:38:50 +0300

    React Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,71 +1,17 @@
 import './App.css';
-import { useReducer } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  // Set range from 0 to 255
-  const limitRGB = (num) => (num < 0 ? 0 : num > 255 ? 255 : num);
-  const step = 51;
+  const [count, setCount] = useState(0);
 
-  const reducer = (state, action) => {
-    switch(action.type){
-      case "INCREASE_RED":
-        return{
-          ...state,
-          red: limitRGB(state.red + step)
-        }
-      case "INCREASE_GREEN":
-        return{
-          ...state,
-          green: limitRGB(state.green + step)
-        }
-      case "INCREASE_BLUE":
-        return{
-          ...state,
-          blue: limitRGB(state.blue + step)
-        }
-      case "DECREASE_RED":
-        return{
-          ...state,
-          red: limitRGB(state.red - step)
-        }
-      case "DECREASE_GREEN":
-        return{
-          ...state,
-          green: limitRGB(state.green - step)
-        }
-      case "DECREASE_BLUE":
-        return{
-          ...state,
-          blue: limitRGB(state.blue - step)
-        }
-    }
-  }
-
-  const [{red,green,blue}, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
-
+  const prevCount = usePrevious(count);
+  
   return(
     <div>
-      <h1 style={{color: `rgb(${red}, ${green}, ${blue})`}}>COLOR TEXT</h1>
-
-      <div>
-        <span>R</span>
-        <button onClick={() => dispatch({type: "INCREASE_RED"})}>+</button>
-        <button onClick={() => dispatch({type: "DECREASE_RED"})}>-</button>
-      </div>
-
-      <div>
-        <span>G</span>
-        <button onClick={() => dispatch({type: "INCREASE_GREEN"})}>+</button>
-        <button onClick={() => dispatch({type: "DECREASE_GREEN"})}>-</button>
-      </div>
-
-      <div>
-        <span>B</span>
-        <button onClick={() => dispatch({type: "INCREASE_BLUE"})}>+</button>
-        <button onClick={() => dispatch({type: "DECREASE_BLUE"})}>-</button>
-      </div>
-
+      <button>Update</button>
+      <h2>Current: {count}</h2>
+      <h2>Previous: {prevCount}</h2>
     </div>
   )
 }
