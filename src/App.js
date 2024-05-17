8df322152643626826e3385d38e771b17865f9bc
Am1d1v <dimadiman1adima@gmail.com>
2024-05-17 20:43:15 +0300

    React usePrevious Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import { usePrevious } from './Hooks/CustomHooks/CustomHook-usePrevious/useprevious';
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
   
   return(
     <div>
-      <button>Update</button>
+      <button onClick={() => setCount(count + 1)}>Update</button>
       <h2>Current: {count}</h2>
       <h2>Previous: {prevCount}</h2>
     </div>
